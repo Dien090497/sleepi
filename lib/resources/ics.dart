6ce49d5e4c65bfcd253761a30daf7bde57c826b8
Sotatek-SonLe2 <son.le2@sotatek.com>
2022-05-25 08:30:15 +0700

    Update assets
    Thêm mới ảnh, icons trong asset

--- lib/resources/ics.dart
+++ lib/resources/ics.dart
@@ -3,19 +3,37 @@ part of 'resources.dart';
 class Ics {
   Ics._();
 
+  static const String arrowLeft = 'assets/icons/arrow_left.svg';
   static const String gacha = 'assets/icons/gacha.svg';
+  static const String icArrowUpRight = 'assets/icons/ic_arrow_up_right.svg';
+  static const String icDownload = 'assets/icons/ic_download.svg';
   static const String icGold = 'assets/icons/ic_gold.svg';
+  static const String icQuestion = 'assets/icons/ic_question.svg';
+  static const String icRefresh = 'assets/icons/ic_refresh.svg';
+  static const String icSetting = 'assets/icons/ic_setting.svg';
   static const String icSilver = 'assets/icons/ic_silver.svg';
   static const String icSolana = 'assets/icons/ic_solana.svg';
   static const String icSolanaCircle = 'assets/icons/ic_solana_circle.svg';
+  static const String icTransfer = 'assets/icons/ic_transfer.svg';
+  static const String icTwoEyes = 'assets/icons/ic_two_eyes.svg';
+  static const String icWallet = 'assets/icons/ic_wallet.svg';
   static const String test = 'assets/icons/test.svg';
 
   static const List<String> values = [
+    arrowLeft,
     gacha,
+    icArrowUpRight,
+    icDownload,
     icGold,
+    icQuestion,
+    icRefresh,
+    icSetting,
     icSilver,
     icSolana,
     icSolanaCircle,
+    icTransfer,
+    icTwoEyes,
+    icWallet,
     test
   ];
 }
