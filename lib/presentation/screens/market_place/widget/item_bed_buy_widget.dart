4ed755b8315b731700d2e7bf004d74b1b29bb75a
thang.bui <thang.bui@sotatek.com>
2022-05-30 18:06:58 +0700

    Update add ui home, tab Jewels, item in Market, item in list

--- lib/presentation/screens/market_place/widget/item_bed_buy_widget.dart
+++ lib/presentation/screens/market_place/widget/item_bed_buy_widget.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:slee_fi/common/routes/app_routes.dart';
 import 'package:slee_fi/common/style/app_colors.dart';
 import 'package:slee_fi/common/style/text_styles.dart';
-import 'package:slee_fi/common/widgets/sf_buttons.dart';
-import 'package:slee_fi/common/widgets/sf_dialog.dart';
 import 'package:slee_fi/common/widgets/sf_text.dart';
-import 'package:slee_fi/presentation/screens/market_place/widget/pop_up_buy_market_place.dart';
 import 'package:slee_fi/presentation/screens/product_detail/widgets/top_left_banner.dart';
 
 class ItemBedBuyWidget extends StatelessWidget {
