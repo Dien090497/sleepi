1b89e9cf5b045fc6c09a4a24948bdef2e458ef8d
M.Dat <dat.thai@sotatek.com>
2022-06-01 10:37:37 +0700

    fix translate()

--- lib/common/widgets/sf_text.dart
+++ lib/common/widgets/sf_text.dart
@@ -1,23 +1,21 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:slee_fi/common/style/text_styles.dart';
 
 class SFText extends Text {
-  const SFText({
+  SFText({
     required String keyText,
     String suffix = '',
     String prefix = '',
-    Map<String, dynamic>? args,
+    Map<String, String>? args,
     TextStyle? style,
     bool toUpperCase = false,
     TextAlign? textAlign,
     Key? key,
   }) : super(
           toUpperCase
-              ? prefix +
-                  // translate(keyText, args: args).toUpperCase() +
-                  suffix
-              // : prefix + translate(keyText, args: args) + suffix,
-              : '',
+              ? prefix + keyText.tr(namedArgs: args).toUpperCase() + suffix
+              : prefix + keyText.tr(namedArgs: args) + suffix,
           style: style ?? TextStyles.textBasic15,
           textAlign: textAlign,
           key: key,
