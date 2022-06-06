38900cdb1255516b8ea39daaa24b0f0efdf4c490
Sotatek-SonLe2 <son.le2@sotatek.com>
2022-06-06 14:55:30 +0700

    Fix multi language

--- lib/presentation/screens/login_signup/widgets/account_login_widget.dart
+++ lib/presentation/screens/login_signup/widgets/account_login_widget.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:slee_fi/common/extensions/string_x.dart';
 import 'package:slee_fi/common/routes/app_routes.dart';
 import 'package:slee_fi/common/style/app_colors.dart';
 import 'package:slee_fi/common/style/text_styles.dart';
 import 'package:slee_fi/common/widgets/sf_buttons.dart';
 import 'package:slee_fi/common/widgets/sf_text.dart';
+import 'package:slee_fi/common/widgets/sf_textfield.dart';
 import 'package:slee_fi/common/widgets/sf_textfield_password.dart';
 import 'package:slee_fi/common/widgets/sf_textfield_text_button.dart';
 import 'package:slee_fi/l10n/locale_keys.g.dart';
@@ -28,10 +30,21 @@ class _AccountLoginState extends State<AccountLoginWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        SFText(
+          keyText: isLoginSignup ? LocaleKeys.login_signup : LocaleKeys.account_login,
+          style: TextStyles.bold18LightWhite,
+          stringCase: StringCase.upperCase,
+        ),
+        const SizedBox(height: 25),
+        const SFTextField(
+          labelText: LocaleKeys.email_address,
+          // hintText: LocaleKeys.emailAddress,
+        ),
+        const SizedBox(height: 12),
         isLoginSignup
             ? const SFTextFieldTextButton(
-                labelText: LocaleKeys.email_verification_code,
-              )
+          labelText: LocaleKeys.email_verification_code,
+        )
             : const SFTextFieldPassword(labelText: LocaleKeys.password,),
         const SizedBox(height: 37),
         SFButton(
@@ -40,8 +53,8 @@ class _AccountLoginState extends State<AccountLoginWidget> {
           textStyle: TextStyles.w600WhiteSize16,
           onPressed: () {
             isLoginSignup ? Navigator.pushNamed(context, R.enterActivationCode)
-                          : Navigator.pushNamedAndRemoveUntil(
-                            context, R.bottomNavigation, (_) => false);
+                : Navigator.pushNamedAndRemoveUntil(
+                context, R.bottomNavigation, (_) => false);
           },
           width: MediaQuery.of(context).size.width,
         ),
