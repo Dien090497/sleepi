d53c87cd4ed16c2195555cda6996ee62d43ae0c6
Sotatek-SonLe2 <son.le2@sotatek.com>
2022-06-07 10:09:46 +0700

    Fix bug

--- lib/common/widgets/sf_app_bar.dart
+++ lib/common/widgets/sf_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:slee_fi/common/extensions/string_x.dart';
 import 'package:slee_fi/common/style/app_colors.dart';
 import 'package:slee_fi/common/widgets/sf_back_button.dart';
 import 'package:slee_fi/common/widgets/sf_text.dart';
