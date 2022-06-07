8a4e572bfe0b2c5babe8cba9cecdecd20593e583
thang.bui <thang.bui@sotatek.com>
2022-06-07 10:56:38 +0700

    Slee - 206, 202, 218

--- lib/presentation/screens/info_individual/layout/recycle_screen.dart
+++ lib/presentation/screens/info_individual/layout/recycle_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:slee_fi/common/extensions/string_x.dart';
 import 'package:slee_fi/common/style/app_colors.dart';
 import 'package:slee_fi/common/style/text_styles.dart';
 import 'package:slee_fi/common/widgets/background_widget.dart';
