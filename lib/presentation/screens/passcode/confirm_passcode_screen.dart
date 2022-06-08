097197d9ce3eab700015446427cb15832af6421b
Duong NV <duong.nguyen3@sotatek.com>
2022-06-08 11:28:06 +0700

    SLEE 217

--- lib/presentation/screens/passcode/confirm_passcode_screen.dart
+++ lib/presentation/screens/passcode/confirm_passcode_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:slee_fi/common/routes/app_routes.dart';
 import 'package:slee_fi/common/style/app_colors.dart';
 import 'package:slee_fi/common/style/text_styles.dart';
 import 'package:slee_fi/common/widgets/background_widget.dart';
