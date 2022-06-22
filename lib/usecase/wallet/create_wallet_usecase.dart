55e7388e4293496451fe5a15ddc7aeb18872c87d
thang.bui <thang.bui@sotatek.com>
2022-06-22 15:28:14 +0700

    create passcode

--- lib/usecase/wallet/create_wallet_usecase.dart
+++ lib/usecase/wallet/create_wallet_usecase.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:slee_fi/entities/wallet_info/wallet_info_entity.dart';
 import 'package:slee_fi/failures/failure.dart';
-import 'package:slee_fi/repository/implementations/wallet_repository.dart';
+import 'package:slee_fi/repository/wallet_repository.dart';
 import 'package:slee_fi/usecase/usecase.dart';
 
 class CreateWalletUseCase extends UseCase<WalletInfoEntity, NoParams> {
