8eee21b8f66db8552a56d7c208cce96d031a925a
M.Dat <dat.thai@sotatek.com>
2022-06-23 15:43:30 +0700

    .gitignore is now working

--- lib/secret.dart
+++
@@ -1,4 +0,0 @@
-const String infuraProjectId = '8a447729876b4f0aafde69982df69709';
-const String ethPrivateKey =
-    '000673dbcfdb4c09ee311915b22f4e3b552b571108e97825d6273ee7c87307fc';
-const String hasuraAdminKey = 'QEduvlhzwY71NByw0litRtGwycLSFGVmtcrr820d7LUyu57tm2ALOV8rL9GDJ5Xh';
