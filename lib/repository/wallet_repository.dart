f89ba59bf68ea870e1b2aca234fbdb00a477e9a6
M.Dat <dat.thai@sotatek.com>
2022-06-23 09:45:04 +0700

    fix network error

--- lib/repository/wallet_repository.dart
+++ lib/repository/wallet_repository.dart
@@ -2,9 +2,12 @@ import 'package:dartz/dartz.dart';
 import 'package:slee_fi/entities/wallet_info/wallet_info_entity.dart';
 import 'package:slee_fi/failures/failure.dart';
 
-abstract class IWalletRepository{
+abstract class IWalletRepository {
   Future<Either<Failure, WalletInfoEntity>> createWallet();
+
   Future<Either<Failure, WalletInfoEntity>> currentWallet();
+
   Future<Either<Failure, WalletInfoEntity>> importWallet(String mnemonic);
+
   Future<Either<Failure, bool>> swapToken();
 }
