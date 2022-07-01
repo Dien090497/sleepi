4c095ee00044fa8c53c455c19e593bf6781e3792
Duong NV <duong.nguyen3@sotatek.com>
2022-07-01 09:10:32 +0700

    update popup warning bind wallet

--- lib/presentation/screens/wallet_creation_warning/widgets/pop_up_avalanche_wallet.dart
+++ lib/presentation/screens/wallet_creation_warning/widgets/pop_up_avalanche_wallet.dart
@@ -72,7 +72,10 @@ class PopUpAvalancheWallet extends StatelessWidget {
   }
 
   _showWarningDialog(BuildContext context) async {
-    await showCustomAlertDialog(context,
-        children: const PopUpWarningBindWallet());
+    await showCustomAlertDialog(
+      context,
+      showClosed: false,
+      children: const PopUpWarningBindWallet(),
+    );
   }
 }
