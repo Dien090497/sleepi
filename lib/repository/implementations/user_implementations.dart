c3c88e8813ad1d87824c86c67b224b383f0cbfb6
Duong NV <duong.nguyen3@sotatek.com>
2022-07-07 15:52:19 +0700

    fetch tokens in spending

--- lib/repository/implementations/user_implementations.dart
+++ lib/repository/implementations/user_implementations.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
-import 'package:slee_fi/common/extensions/string_x.dart';
 import 'package:slee_fi/datasources/remote/auth_datasource/auth_datasource.dart';
 import 'package:slee_fi/entities/active_code/active_code_entity.dart';
 import 'package:slee_fi/failures/failure.dart';
+import 'package:slee_fi/models/token_spending/token_spending.dart';
 import 'package:slee_fi/repository/user_repository.dart';
 import 'package:slee_fi/schema/change_password_schema/change_password_schema.dart';
 
@@ -36,11 +36,10 @@ class UserImplementation extends IUserRepository {
   }
 
   @override
-  Future<Either<FailureMessage, dynamic>> fetchBalanceSpending(
+  Future<Either<FailureMessage, List<TokenSpending>>> fetchBalanceSpending(
       String userID) async {
     try {
       var result = await _authDataSource.fetchBalanceSpending(userID);
-      'get balance spending $result'.log;
       return Right(result);
     } on Exception catch (e) {
       return Left(FailureMessage.fromException(e));
--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:slee_fi/entities/active_code/active_code_entity.dart';
 import 'package:slee_fi/failures/failure.dart';
+import 'package:slee_fi/models/token_spending/token_spending.dart';
 import 'package:slee_fi/schema/change_password_schema/change_password_schema.dart';
 
 abstract class IUserRepository {
@@ -8,7 +9,7 @@ abstract class IUserRepository {
 
   Future<Either<FailureMessage, List<ActiveCodeEntity>>> fetchActivationCodes();
 
-  Future<Either<FailureMessage, dynamic>> fetchBalanceSpending(String userID);
+  Future<Either<FailureMessage, List<TokenSpending>>> fetchBalanceSpending(String userID);
 
 
 }
