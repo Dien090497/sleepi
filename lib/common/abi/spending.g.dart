e61c02f39cb838caeaf6e29f5f2ee821afd09ab8
M.Dat <dat.thai@sotatek.com>
2022-07-09 14:03:33 +0700

    rerun build runner

---
+++ lib/common/abi/spending.g.dart
@@ -0,0 +1,127 @@
+// Generated code, do not modify. Run `build_runner build` to re-generate!
+// @dart=2.12
+import 'package:web3dart/web3dart.dart' as _i1;
+
+final _contractAbi = _i1.ContractAbi.fromJson(
+    '[{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint8","name":"version","type":"uint8"}],"name":"Initialized","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"userId","type":"uint256"},{"indexed":true,"internalType":"address","name":"collection","type":"address"},{"indexed":false,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"LogLockNft","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"userId","type":"uint256"},{"indexed":true,"internalType":"address","name":"token","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"LogLockToken","type":"event"},{"inputs":[{"internalType":"address","name":"collection","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"userId","type":"uint256"}],"name":"depositNft","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"userId","type":"uint256"}],"name":"depositToken","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"_admin","type":"address"}],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]',
+    'Spending');
+
+class Spending extends _i1.GeneratedContract {
+  Spending(
+      {required _i1.EthereumAddress address,
+      required _i1.Web3Client client,
+      int? chainId})
+      : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
+
+  /// The optional [transaction] parameter can be used to override parameters
+  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
+  /// set by the contract.
+  Future<String> depositNft(
+      _i1.EthereumAddress collection, BigInt tokenId, BigInt userId,
+      {required _i1.Credentials credentials,
+      _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[0];
+    assert(checkSignature(function, 'e09a3cb4'));
+    final params = [collection, tokenId, userId];
+    return write(credentials, transaction, function, params);
+  }
+
+  /// The optional [transaction] parameter can be used to override parameters
+  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
+  /// set by the contract.
+  Future<String> depositToken(
+      _i1.EthereumAddress token, BigInt amount, BigInt userId,
+      {required _i1.Credentials credentials,
+      _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[1];
+    assert(checkSignature(function, '99c6d2de'));
+    final params = [token, amount, userId];
+    return write(credentials, transaction, function, params);
+  }
+
+  /// The optional [transaction] parameter can be used to override parameters
+  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
+  /// set by the contract.
+  Future<String> initialize(_i1.EthereumAddress _admin,
+      {required _i1.Credentials credentials,
+      _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[2];
+    assert(checkSignature(function, 'c4d66de8'));
+    final params = [_admin];
+    return write(credentials, transaction, function, params);
+  }
+
+  /// Returns a live stream of all Initialized events emitted by this contract.
+  Stream<Initialized> initializedEvents(
+      {_i1.BlockNum? fromBlock, _i1.BlockNum? toBlock}) {
+    final event = self.event('Initialized');
+    final filter = _i1.FilterOptions.events(
+        contract: self, event: event, fromBlock: fromBlock, toBlock: toBlock);
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(result.topics!, result.data!);
+      return Initialized(decoded);
+    });
+  }
+
+  /// Returns a live stream of all LogLockNft events emitted by this contract.
+  Stream<LogLockNft> logLockNftEvents(
+      {_i1.BlockNum? fromBlock, _i1.BlockNum? toBlock}) {
+    final event = self.event('LogLockNft');
+    final filter = _i1.FilterOptions.events(
+        contract: self, event: event, fromBlock: fromBlock, toBlock: toBlock);
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(result.topics!, result.data!);
+      return LogLockNft(decoded);
+    });
+  }
+
+  /// Returns a live stream of all LogLockToken events emitted by this contract.
+  Stream<LogLockToken> logLockTokenEvents(
+      {_i1.BlockNum? fromBlock, _i1.BlockNum? toBlock}) {
+    final event = self.event('LogLockToken');
+    final filter = _i1.FilterOptions.events(
+        contract: self, event: event, fromBlock: fromBlock, toBlock: toBlock);
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(result.topics!, result.data!);
+      return LogLockToken(decoded);
+    });
+  }
+}
+
+class Initialized {
+  Initialized(List<dynamic> response) : version = (response[0] as BigInt);
+
+  final BigInt version;
+}
+
+class LogLockNft {
+  LogLockNft(List<dynamic> response)
+      : user = (response[0] as _i1.EthereumAddress),
+        userId = (response[1] as BigInt),
+        collection = (response[2] as _i1.EthereumAddress),
+        tokenId = (response[3] as BigInt);
+
+  final _i1.EthereumAddress user;
+
+  final BigInt userId;
+
+  final _i1.EthereumAddress collection;
+
+  final BigInt tokenId;
+}
+
+class LogLockToken {
+  LogLockToken(List<dynamic> response)
+      : user = (response[0] as _i1.EthereumAddress),
+        userId = (response[1] as BigInt),
+        token = (response[2] as _i1.EthereumAddress),
+        amount = (response[3] as BigInt);
+
+  final _i1.EthereumAddress user;
+
+  final BigInt userId;
+
+  final _i1.EthereumAddress token;
+
+  final BigInt amount;
+}
---
+++ lib/schema/refresh_token_schema/refresh_token_schema.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'refresh_token_schema.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RefreshTokenSchema _$RefreshTokenSchemaFromJson(Map<String, dynamic> json) =>
+    RefreshTokenSchema(
+      json['refreshToken'] as String,
+    );
+
+Map<String, dynamic> _$RefreshTokenSchemaToJson(RefreshTokenSchema instance) =>
+    <String, dynamic>{
+      'refreshToken': instance.refreshToken,
+    };
