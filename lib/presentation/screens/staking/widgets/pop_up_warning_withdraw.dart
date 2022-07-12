d8d79daef733744e3fa9b8cbac8b411295dc45df
Sotatek-SonLe2 <son.le2@sotatek.com>
2022-07-12 17:05:18 +0700

    Fix bug on staking

---
+++ lib/presentation/screens/staking/widgets/pop_up_warning_withdraw.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:slee_fi/common/style/app_colors.dart';
+import 'package:slee_fi/common/style/text_styles.dart';
+import 'package:slee_fi/common/widgets/sf_text.dart';
+import 'package:slee_fi/l10n/locale_keys.g.dart';
+
+class PopUpWarningWithdraw extends StatelessWidget {
+  const PopUpWarningWithdraw({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 24),
+      child: Column(
+        children: [
+          const Icon(
+            Icons.warning_amber_outlined,
+            color: AppColors.yellow,
+            size: 32,
+          ),
+          const SizedBox(height: 20),
+          SFText(
+            keyText: LocaleKeys.your_wallet_is_being_created,
+            style: TextStyles.bold18LightWhite,
+          ),
+          const SizedBox(height: 24),
+          SFText(
+            keyText: LocaleKeys.caution_please_check_your_seed_phrase,
+            style: TextStyles.lightGrey16.copyWith(height: 1.5),
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
+}
