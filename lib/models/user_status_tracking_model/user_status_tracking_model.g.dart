93d6df7c140aed5ce9345a03b9b6bd4a9c6537a7
thang.bui <thang.bui@sotatek.com>
2022-07-16 13:42:55 +0700

    SLEE - 468: tracking screen

--- lib/models/user_status_tracking_model/user_status_tracking_model.g.dart
+++ lib/models/user_status_tracking_model/user_status_tracking_model.g.dart
@@ -9,15 +9,17 @@ part of 'user_status_tracking_model.dart';
 UserStatusTrackingModel _$UserStatusTrackingModelFromJson(
         Map<String, dynamic> json) =>
     UserStatusTrackingModel(
-      json['tracking'] as int,
-      json['isAvailable'] as bool,
-      json['availableAt'] as int,
+      tracking: json['tracking'] == null
+          ? null
+          : TrackingModel.fromJson(json['tracking'] as Map<String, dynamic>),
+      isAvailable: json['isAvailable'] as bool,
+      availableAt: json['availableAt'] as int,
     );
 
 Map<String, dynamic> _$UserStatusTrackingModelToJson(
         UserStatusTrackingModel instance) =>
     <String, dynamic>{
-      'tracking': instance.tracking,
+      'tracking': instance.tracking?.toJson(),
       'isAvailable': instance.isAvailable,
       'availableAt': instance.availableAt,
     };
--- lib/schema/sleep_tracking/data_health_schema.g.dart
+++ lib/schema/sleep_tracking/data_health_schema.g.dart
@@ -8,7 +8,6 @@ part of 'data_health_schema.dart';
 
 DataHealthSchema _$DataHealthSchemaFromJson(Map<String, dynamic> json) =>
     DataHealthSchema(
-      trackingId: json['trackingId'] as int?,
       datas: (json['datas'] as List<dynamic>?)
           ?.map((e) => DataHealth.fromJson(e as Map<String, dynamic>))
           .toList(),
@@ -16,6 +15,5 @@ DataHealthSchema _$DataHealthSchemaFromJson(Map<String, dynamic> json) =>
 
 Map<String, dynamic> _$DataHealthSchemaToJson(DataHealthSchema instance) =>
     <String, dynamic>{
-      'trackingId': instance.trackingId,
       'datas': instance.datas?.map((e) => e.toJson()).toList(),
     };
