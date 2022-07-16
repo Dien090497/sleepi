30f437644f50ed6671ee5f57d98832e4c8b6c568
thang.bui <thang.bui@sotatek.com>
2022-07-16 16:45:19 +0700

    update sleep tracking

--- lib/datasources/remote/sleep_tracking_api/sleep_tracking_api.dart
+++ lib/datasources/remote/sleep_tracking_api/sleep_tracking_api.dart
@@ -6,6 +6,7 @@ import 'package:slee_fi/common/const/const.dart';
 import 'package:slee_fi/datasources/remote/auth_datasource/auth_interceptor.dart';
 import 'package:slee_fi/datasources/remote/auth_datasource/refresh_token_interceptor.dart';
 import 'package:slee_fi/models/estimate_sleep_response/estimate_sleep_response.dart';
+import 'package:slee_fi/models/tracking_result_chart/tracking_result_model.dart';
 import 'package:slee_fi/models/user_status_tracking_model/user_status_tracking_model.dart';
 import 'package:slee_fi/schema/sleep_tracking/data_health_schema.dart';
 import 'package:slee_fi/schema/start_tracking/start_tracking_schema.dart';
@@ -45,7 +46,7 @@ abstract class SleepTrackingApi {
   Future<String> getResult(@Path('id') int id);
 
   @POST('/tracking/wake-up')
-  Future<dynamic> wakeUp(@Body() DataHealthSchema dataHealthSchema);
+  Future<TrackingResultModel> wakeUp(@Body() DataHealthSchema dataHealthSchema);
 
   @GET('/tracking/user-status-tracking')
   Future<UserStatusTrackingModel> userStatusTracking();
--- lib/presentation/screens/info_individual/widget/box_info_widget.dart
+++ lib/presentation/screens/info_individual/widget/box_info_widget.dart
@@ -32,6 +32,7 @@ class BoxInfoWidget extends StatelessWidget {
               textStyle: TextStyles.lightWhite14,
             ),
             const SizedBox(height: 16),
+            if(bed.startTime!=null)
             Text(
               '${LocaleKeys.time.tr()}: ${bed.startTime!.removeTrailingZeros}h - ${bed.endTime!.removeTrailingZeros}h',
               style: TextStyles.lightGrey14,
