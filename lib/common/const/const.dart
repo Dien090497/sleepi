c9de02559dfbe7ac7fb7f4682ba6305146732065
Duong NV <duong.nguyen3@sotatek.com>
2022-07-18 17:50:59 +0700

    add option latest to filter market

--- lib/common/const/const.dart
+++ lib/common/const/const.dart
@@ -67,7 +67,7 @@ class Const {
     localeRU,
     localeZH,
   ];
-
+  static const sortCondition = ["LowPrice", "HighPrice", "Latest"];
   static const tokens = [
     {
       // "address": "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",//mainnet
@@ -164,7 +164,6 @@ class Const {
 
   Const._();
 }
-
 
 extension LocaleX on Locale {
   String get displayName {
