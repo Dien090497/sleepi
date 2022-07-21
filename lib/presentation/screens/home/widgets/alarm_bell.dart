251731a106fcc1316063482fb664b76aeb360740
M.Dat <dat.thai@sotatek.com>
2022-07-21 08:45:33 +0700

    add refresh after back from tracking

--- lib/presentation/screens/home/widgets/alarm_bell.dart
+++ lib/presentation/screens/home/widgets/alarm_bell.dart
@@ -211,7 +211,8 @@ class AlarmBell extends StatelessWidget {
         context,
         children: PopUpConfirmStartTracking(
           onPressed: () async {
-            final state = BlocProvider.of<HomeBloc>(context).state;
+            final homeBloc = BlocProvider.of<HomeBloc>(context);
+            final state = homeBloc.state;
             if (state is HomeLoaded) {
               final startRes = await getIt<StartSleepTrackingUseCase>()
                   .call(StartTrackingSchema(
@@ -238,7 +239,9 @@ class AlarmBell extends StatelessWidget {
                       fromRoute: R.bottomNavigation,
                       imageBed: bedImage,
                     ),
-                  );
+                  ).then((_) {
+                    homeBloc.add(const RefreshBed());
+                  });
                 },
               );
             }
