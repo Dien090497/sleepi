973688c6d1b3469fc7a8dcccf82585bc84c47c1c
Sotatek-SonLe2 <son.le2@sotatek.com>
2022-07-26 15:39:14 +0700

    Fix gacha

--- lib/l10n/locale_keys.g.dart
+++ lib/l10n/locale_keys.g.dart
@@ -477,5 +477,6 @@ abstract class  LocaleKeys {
   static const mint_fail = 'mint_fail';
   static const please_turn_on_tracking = 'please_turn_on_tracking';
   static const taking_off_jewel_with_cost = 'taking_off_jewel_with_cost';
+  static const genenis_common_bed = 'genenis_common_bed';
 
 }
