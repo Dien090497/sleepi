b794e383bb0062c7f6cfe85829a19832e07645a9
thang.bui <thang.bui@sotatek.com>
2022-07-30 17:12:49 +0700

    Update save list token global config

--- lib/usecase/approve_usecase.dart
+++ lib/usecase/approve_usecase.dart
@@ -1,30 +1,17 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:slee_fi/failures/failure.dart';
-import 'package:slee_fi/repository/spending_repository.dart';
 import 'package:slee_fi/repository/wallet_repository.dart';
 import 'package:slee_fi/usecase/usecase.dart';
 
 @injectable
-class ApproveUseCase extends UseCase<String, String> {
-  final ISpendingRepository _iSpendingRepository;
+class ApproveUseCase extends UseCase<bool, String> {
   final IWalletRepository _iWalletRepository;
 
-  ApproveUseCase(this._iSpendingRepository, this._iWalletRepository);
+  ApproveUseCase(this._iWalletRepository);
 
   @override
-  Future<Either<FailureMessage, String>> call(String params) async {
-    final currentWalletRes = await _iWalletRepository.currentWallet();
-    return currentWalletRes.fold((l) {
-      return Left(FailureMessage("$l"));
-    }, (r) async {
-      final result = await _iSpendingRepository.approve(
-          owner: r.credentials, addressContract: params);
-      return result.fold((l) {
-        return Left(FailureMessage("$l"));
-      }, (r) {
-        return Right(r);
-      });
-    });
+  Future<Either<Failure, bool>> call(String params) async {
+    return _iWalletRepository.approveToken(params);
   }
 }
