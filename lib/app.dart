036b1de16724182d6a0bcf3f2b15ca6a0f10b09f
thang.bui <thang.bui@sotatek.com>
2022-07-30 19:05:47 +0700

    fix bug 886

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -7,11 +8,15 @@ import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:slee_fi/common/routes/app_routes.dart';
 import 'package:slee_fi/common/style/theme.dart';
 import 'package:slee_fi/common/widgets/phoenix.dart';
+import 'package:slee_fi/l10n/locale_keys.g.dart';
 import 'package:slee_fi/presentation/blocs/bottom_navigation/bottom_navigation_bloc.dart';
 import 'package:slee_fi/presentation/blocs/global_listener/global_listener_cubit.dart';
 import 'package:slee_fi/presentation/blocs/refresh_cubit/refresh_cubit.dart';
 import 'package:slee_fi/presentation/blocs/user_bloc/user_bloc.dart';
 import 'package:slee_fi/presentation/blocs/wallet/wallet_cubit.dart';
+
+import 'common/style/text_styles.dart';
+import 'common/widgets/sf_text.dart';
 
 final RouteObserver<ModalRoute<void>> routeObserver =
     RouteObserver<ModalRoute<void>>();
@@ -35,6 +40,38 @@ class MyApp extends StatelessWidget {
           builder: (_, child) {
             return RefreshConfiguration(
               headerBuilder: () => const WaterDropHeader(),
+              footerBuilder: () => CustomFooter(
+                builder: (BuildContext context, LoadStatus? mode) {
+                  Widget body;
+                  if (mode == LoadStatus.idle) {
+                    body = SFText(
+                      keyText: LocaleKeys.pull_up_load,
+                      style: TextStyles.lightWhite16,
+                    );
+                  } else if (mode == LoadStatus.loading) {
+                    body = const CupertinoActivityIndicator();
+                  } else if (mode == LoadStatus.failed) {
+                    body = SFText(
+                      keyText: LocaleKeys.load_fail_retry,
+                      style: TextStyles.lightWhite16,
+                    );
+                  } else if (mode == LoadStatus.canLoading) {
+                    body = SFText(
+                      keyText: LocaleKeys.release_to_load_more,
+                      style: TextStyles.lightWhite16,
+                    );
+                  } else {
+                    body = SFText(
+                      keyText: LocaleKeys.no_more_data,
+                      style: TextStyles.lightWhite16,
+                    );
+                  }
+                  return SizedBox(
+                    height: 55.0,
+                    child: Center(child: body),
+                  );
+                },
+              ),
               child: MaterialApp(
                 // key: Key(context.locale.languageCode),
                 title: 'SleeFi',
