4ef3940a4ba2c4f064f5a79192d0a5ad4d01bb24
thang.bui <thang.bui@sotatek.com>
2022-08-04 12:31:35 +0700

    Fix bug trade slft to other when new wallet

--- lib/presentation/blocs/trade/trade_cubit.dart
+++ lib/presentation/blocs/trade/trade_cubit.dart
@@ -12,7 +12,7 @@ import 'package:slee_fi/usecase/usecase.dart';
 class TradeCubit extends Cubit<TradeState> {
   TradeCubit()
       : super(TradeStateInitial(
-      listTokens: getIt<List<dynamic>>(instanceName: 'tokens')));
+            listTokens: getIt<List<dynamic>>(instanceName: 'tokens')));
 
   final _swapToken = getIt<SwapTokenUseCase>();
   final _approveToken = getIt<ApproveTradeTokenUseCase>();
@@ -21,37 +21,39 @@ class TradeCubit extends Cubit<TradeState> {
   final _getAmountOutMin = getIt<GetAmountOutMinUseCase>();
 
   Future<void> init() async {
-
     await getIt<GetListTokenUseCase>().call(NoParams()).then((value) => {
-      value.fold((l) {
-        emit(TradeState.initial(
-            listTokens: getIt<List<dynamic>>(instanceName: 'tokens'), isLoading: true));
-      }, (r) {
-        emit(TradeState.initial(listTokens: r, isLoading: true));
-      })
-    });
+          value.fold((l) {
+            emit(TradeState.initial(
+                listTokens: getIt<List<dynamic>>(instanceName: 'tokens'),
+                isLoading: true));
+          }, (r) {
+            emit(TradeState.initial(listTokens: r, isLoading: true));
+          })
+        });
   }
 
   Future<void> swapToken(double value, String contractAddressFrom,
       String contractAddressTo) async {
     emit(const TradeState.loading());
-    final result = await _swapToken.call(SwapTokenParams(
-        value: value,
-        contractAddressFrom: contractAddressFrom,
-        contractAddressTo: contractAddressTo));
-
-    result.fold(
-          (l) {
-        emit(TradeState.fail('$l'));
-      },
-          (r) {
-        if(r=='') {
-          emit(const TradeState.success());
-        }else{
-          emit(TradeState.fail(r));
-        }
-      },
-    );
+    await _swapToken
+        .call(SwapTokenParams(
+            value: value,
+            contractAddressFrom: contractAddressFrom,
+            contractAddressTo: contractAddressTo))
+        .then((value) {
+      value.fold(
+        (l) {
+          emit(TradeState.fail('$l'));
+        },
+        (r) {
+          if (r == '') {
+            emit(const TradeState.success());
+          } else {
+            emit(TradeState.fail(r));
+          }
+        },
+      );
+    });
   }
 
   Future<void> approveToken(String contractAddress) async {
@@ -59,10 +61,10 @@ class TradeCubit extends Cubit<TradeState> {
     final result = await _approveToken.call(contractAddress);
     Future.delayed(const Duration(milliseconds: 1000), () {
       result.fold(
-            (l) {
+        (l) {
           emit(TradeState.fail('$l'));
         },
-            (txh) {
+        (txh) {
           emit(TradeState.approveSuccess(txh));
         },
       );
@@ -73,10 +75,10 @@ class TradeCubit extends Cubit<TradeState> {
     final result = await _checkAllowance.call(CheckApproveTokenParams(
         value: value, contractAddress: contractAddress));
     result.fold(
-          (l) {
+      (l) {
         emit(TradeState.fail('$l'));
       },
-          (success) {
+      (success) {
         emit(TradeState.allowance(success));
       },
     );
@@ -85,10 +87,10 @@ class TradeCubit extends Cubit<TradeState> {
   Future<void> getBalanceToken(String contractAddress) async {
     final result = await _getBalanceToken.call(contractAddress);
     result.fold(
-          (l) {
+      (l) {
         emit(TradeState.fail('$l'));
       },
-          (success) {
+      (success) {
         emit(TradeState.getBalance(success));
       },
     );
@@ -101,10 +103,10 @@ class TradeCubit extends Cubit<TradeState> {
         contractAddressFrom: contractAddressFrom,
         contractAddressTo: contractAddressTo));
     result.fold(
-          (l) {
+      (l) {
         emit(TradeState.fail('$l'));
       },
-          (success) {
+      (success) {
         emit(TradeState.getAmountOutMin(success));
       },
     );
